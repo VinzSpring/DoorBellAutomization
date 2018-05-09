341f448ce21c9ac6e71201f709095bf68723638d
Manuel <s3866577@msx.tu-dresden.de>
2018-05-09 04:07:20 +0200

    TokenSystem works

--- engine.py
+++ engine.py
@@ -1,29 +1,56 @@
+import datetime
+from threading import Thread, Timer
+
+from door_bell_controls import DoorBellControls
 from tokens import TokenManager
+from time import sleep, time
+
+DEFAULT_TIME_FOR_RING_AND_OPEN = 3  # seconds
+DEFAULT_EXPIRE_TIME_TOKENS = 20  # minutes
 
 
 class Engine:
 
-    def __init__(self, door_bell_controller):
-        self.door_bell_controller = door_bell_controller
+    def __init__(self, door_bell_controls):
+        self.door_bell_controls = door_bell_controls
         self.on_ringed_notification = None
         self.on_opened_door_notification = None
         self.open_always = False
         self.token_manager = TokenManager()
+        self.timer = None
         pass
 
     def start(self):
-        self.door_bell_controller.watch_door_bell_ring(self.on_ring())
+        self.door_bell_controls.watch_door_bell_ring(self.on_ring)
         pass
+
+    def insert_token(self, token=None):
+        expire_time = DEFAULT_EXPIRE_TIME_TOKENS*1000
+        if token:
+            self.token_manager.insert_token(token, expire_time)
+            return token
+        else:
+            return self.token_manager.insert_random_token(expire_time)
 
     def activate_token(self, token):
         if self.token_manager.token_is_valid(token):
             self.token_manager.delete_token(token)
+            self.open_always = True
             self.reset_timer_for_token()
+        else:
+            print("Access denied: Token was not valid or expired")
+
+    def disable_always_open(self):
+        self.open_always = False
 
     def reset_timer_for_token(self):
-        self.open_always = True
+        # cancel timer if exist
+        if self.timer:
+            self.timer.cancel()
+
         # start timer and after that
-        self.open_always = False
+        self.timer = Timer(DEFAULT_TIME_FOR_RING_AND_OPEN, self.disable_always_open)
+        self.timer.start()
         pass
 
     def on_ring(self):
@@ -37,9 +64,9 @@ class Engine:
         pass
 
     def open_door(self):
-        self.door_bell_controller.open_door()
-        if light_is_needed():
-            self.door_bell_controller.turn_lights_on()
+        self.door_bell_controls.open_door()
+        if self.light_is_needed():
+            self.door_bell_controls.turn_lights_on()
         pass
 
     def set_on_ringed_notification(self, callback):
@@ -50,6 +77,35 @@ class Engine:
         self.on_opened_door_notification = callback
         pass
 
+    @staticmethod
+    def light_is_needed():
+        return False
 
-def light_is_needed():
-    return False
+
+
+
+
+# EXAMPLE PROGRAM
+def simulate_guest():
+    engine.activate_token("pass")
+    new_timer = Timer(5, lambda: dbc.on_ring())
+    new_timer.start()
+
+
+if __name__ == '__main__':
+    dbc = DoorBellControls()
+    engine = Engine(dbc)
+    engine.set_on_opened_door_notification(lambda: print("NOTIFICATION: Opened the door"))
+    engine.set_on_ringed_notification(lambda: print("NOTIFICATION: It rings"))
+    engine.start()
+
+    engine.insert_token("pass")
+    timer = Timer(5, simulate_guest)
+    timer.start()
+    pass
+
+
+
+
+
+
