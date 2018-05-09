0d90ed47c6c0b77206f04ebc542d7c1b9c7db632
Manuel <s3866577@msx.tu-dresden.de>
2018-05-09 04:07:20 +0200

    TokenSystem works

--- door_bell_controls.py
+++ door_bell_controls.py
@@ -1,15 +1,19 @@
-from hardware_watcher import ImportWatcher
+# from hardware_watcher import ImportWatcher
 
 
-class DoorBellController:
+class DoorBellControls:
 
     def __init__(self):
-        self.hooked_pins = ImportWatcher()
+        # self.hooked_pins = ImportWatcher()
+        self.hooked_pins = None
+        self.on_ring_callback = None
 
     def open_door(self):
+        print("Opening the door...")
         pass
 
     def toggle_bell_tone(self, enabled):
+        print("Setting bell tone to " + enabled)
         pass
 
     def send_audio(self, audio):
@@ -22,11 +26,17 @@ class DoorBellController:
         pass
 
     def watch_door_bell_ring(self, callback):
+        self.on_ring_callback = callback
         # watch the pin
         # callback()
         pass
 
+    def on_ring(self):
+        if self.on_ring_callback:
+            self.on_ring_callback()
+
     def turn_lights_on(self):
+        print("Turning the lights on")
         pass
 
 
--- tokens.py
+++ tokens.py
@@ -10,25 +10,29 @@ class TokenManager:
     def __init__(self):
         self.tokens = {}
 
-    def insert_random_token(self, expire_date):
+    def insert_random_token(self, expire_time):
         random_token = "abc"
-        self.insert_token(random_token, expire_date)
+        self.insert_token(random_token, expire_time)
         return random_token
 
-    def insert_token(self, token, expire_date):
-        self.tokens[token] = expire_date
-
-    def token_is_valid(self, token):
-        if token in self.tokens:
-            return not token_expired(self.tokens[token])
-        return False
+    def insert_token(self, token, valid_time):
+        now = time.time()
+        self.tokens[token] = now + valid_time
 
     def delete_token(self, token):
-        self.tokens[token] = None
+        del self.tokens[token]
+
+    def token_is_valid(self, token):
+        self.check_all_tokens()
+        if token in self.tokens:
+            valid = not token_expired(self.tokens[token])
+            # delete token if you want to delete if immediately after succeeded check
+            return valid
+        return False
 
     def check_all_tokens(self):
         delete_list = []
-        for token, exp_date in self.tokens:
+        for token, exp_date in self.tokens.items():
             if token_expired(exp_date):
                 delete_list.append(token)
 
