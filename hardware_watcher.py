9af0479bfc7326179908710b4d9b2c363f4e4d90
Manuel <s3866577@msx.tu-dresden.de>
2018-05-09 12:13:39 +0200

    Optimized Code

--- hardware_watcher.py
+++ hardware_watcher.py
@@ -7,17 +7,17 @@ class HookedPin:
         self.hooked = False
         self.callback = callback
 
-    def send(self,delay):
+    def send(self, delay):
         pass
+
 
 class ImportWatcher:
 
     def __init__(self):
         self.hooked_pins = {}
 
-
-    def hook_gpio(self, hookedPin):
-        self.hooked_pins[hookedPin.pin] = hookedPin
+    def hook_gpio(self, hooked_pin):
+        self.hooked_pins[hooked_pin.pin] = hooked_pin
 
     def refresh(self):
         for pin, hookedPin in self.hooked_pins:
